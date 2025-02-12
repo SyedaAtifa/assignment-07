2e5116e92c41c1b0d9f3e80c6256b81693439e32
Syeda Atifa <syedaatifa4307@gmail.com>
2025-02-13 00:16:37 +0500

    Update page.tsx

--- app/server-side/page.tsx
+++ app/server-side/page.tsx
@@ -21,7 +21,7 @@ const Page = async () => {
 
         <h2 className="w-screen h-16 flex items-center justify-center font-bold text-4xl mt-8 text-[#978686]">Server Side</h2>
       {parsedResponse.map ((books) => (
-       <div key={book.id} className="h-[180px] border rounded-[10px] m-6 flex flex-col gap-2 justify-center items-center text-[#474747] text-xl bg-[#e6dddd] transition-transform duration-100 hover:scale-110 shadow-xl">
+       <div key={books.id} className="h-[180px] border rounded-[10px] m-6 flex flex-col gap-2 justify-center items-center text-[#474747] text-xl bg-[#e6dddd] transition-transform duration-100 hover:scale-110 shadow-xl">
           <div  className="h-[180px] border rounded-[10px] m-6 flex flex-col gap-2 justify-center items-center">
             <p>Book: {books.id}</p>
             <p className="font-bold text-3xl text-[#2e4a7e]">{books.name}</p>
